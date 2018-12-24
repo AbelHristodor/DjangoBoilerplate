19033e5c1b6ed17a67a5fc533669c9c07b3cd159
Abel Hristodor <habelor@gmail.com>
2018-12-24 16:53:16 +0100

    Added html base structure and homepage

--- boilerplate/views.py
+++ boilerplate/views.py
@@ -1,8 +1,14 @@
-from .tasks import send_msg_task
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .tasks import send_msg_task
+
 import json
 
 def send_msg_view(request):
     message = "Hello World"
     send_msg_task.delay(message)
     return HttpResponse(json.dumps({"success": True, "message": message}))
+
+def home_view(request):
+    return render(request, "pages/homepage.html")
